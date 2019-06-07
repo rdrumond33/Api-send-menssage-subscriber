9e80c4c104416d7f258e93c01ab60c61af5fe102
Rodrigo Drumond <r-drumond@hotmail.com>
2019-06-07 10:01:13 -0300

    deploy heroku
    deploy heroku

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,14 +1,13 @@
 import { Sequelize } from "sequelize";
 
-/*
+
 
 const URI = "postgres://aqbumuordtkrdp:6407a66a52a83abcd2fce84dfa64f162fcbe7f9e4e19ca917881688be2f6d513@ec2-54-83-36-37.compute-1.amazonaws.com:5432/dajbhe6ba7p44d"
 
 const sequelize = new Sequelize(URI);
 
-*/
 
-
+/*
 const DATA = "db_mensages_assinantes";
 const USERNAME = 'postgres';
 const PASSWORD = "root";
@@ -26,7 +25,7 @@ const sequelize = new Sequelize(DATA, USERNAME, PASSWORD, {
 });
 
 
-
+*/
 
 sequelize.sync();
 
